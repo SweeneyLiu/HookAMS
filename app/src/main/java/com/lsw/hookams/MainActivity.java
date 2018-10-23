ccad0cd395b327fee93d96884fad1946914f62bd
liushuwei <lsw305264677@gmail.com>
2018-10-23 17:01:04 +0800

    挪出文件

--- app/src/main/java/com/lsw/hookams/MainActivity.java
+++ app/src/main/java/com/lsw/hookams/MainActivity.java
@@ -37,37 +37,4 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });    }
-
-    /**
-     * Created by sweeneyliu on 2018/10/22.
-     */
-    public static final class HookHelper {
-
-        public static void hookActivityManager() {
-            try {
-                //获取AMN的gDefault单例gDefault，gDefault是静态的
-                Object gDefault = RefInvoke.getFieldObject("android.app.ActivityManagerNative", null,"gDefault");
-
-                // gDefault是一个 android.util.Singleton对象; 我们取出这个单例里面的mInstance字段，IActivityManager类型
-                Object rawIActivityManager = RefInvoke.getFieldObject(
-                        "android.util.Singleton",
-                        gDefault, "mInstance");
-
-
-                // 创建一个这个对象的代理对象iActivityManagerInterface, 然后替换这个字段, 让我们的代理对象帮忙干活
-                Class<?> iActivityManagerInterface = Class.forName("android.app.IActivityManager");
-                Object proxy = Proxy.newProxyInstance(
-                        Thread.currentThread().getContextClassLoader(),
-                        new Class<?>[] { iActivityManagerInterface },
-    //                    new HookHandler(rawIActivityManager));
-                        new HookStartActivityHandler(rawIActivityManager));
-
-                //把Singleton的mInstance替换为proxy
-                RefInvoke.setFieldObject("android.util.Singleton", gDefault, "mInstance", proxy);
-
-            } catch (Exception e) {
-                throw new RuntimeException("Hook Failed", e);
-            }
-        }
-    }
 }
